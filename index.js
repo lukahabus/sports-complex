f76c85cd7f710621c3461a3f8e1ea0755b920ed7
Luka Habuš <habusluka@gmail.com>
2022-02-16 13:14:55 +0100

    Added RESTful API

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ const PORT = 8080;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', async (request, response) => {
-
     response.send( await readFile('./home.html', 'utf-8') );
+});
 
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'large'
+    }) 
+});
+
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if(!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `\u{1F455} with your ${logo} logo and ID of ${id}`,
+    });
 });
 
 app.listen(PORT, () => console.log(`App available on http://localhost:${PORT}`));
